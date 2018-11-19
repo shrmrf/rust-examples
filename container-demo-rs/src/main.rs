6d16ffb194118fb578fe915f322142db10a3fc46
T <shrmrf@users.noreply.github.com>
2018-11-19 15:30:00 +0100

    pass slice to `run()` Todo:  complete run and create `child()`

--- container-demo-rs/src/main.rs
+++ container-demo-rs/src/main.rs
@@ -10,10 +10,15 @@ fn main() {
 
     if args[1] == "run" {
         let arguments = &args[1..];
-        println!("run main process here, with args: {:?}", arguments);
+        println!("run main process here, with args: {:?}", &arguments[1..]);
+        run(&arguments[1..]);
     } else if args[1] == "child" {
         println!("run child process here");
     } else {
         print_usage();
     }
+}
+
+fn run(run_args: &[String]) {
+    println!("Arguments: {:?}", run_args);
 }
